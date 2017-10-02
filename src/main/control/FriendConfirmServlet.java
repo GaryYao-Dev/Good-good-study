9e1de6df44f3f88dcd4fcd1b374d035a8e747648
jiewang-wj <31268741+jiewang-wj@users.noreply.github.com>
2017-10-02 23:53:23 +1100

    Add files via upload

--- src/main/control/FriendConfirmServlet.java
+++ src/main/control/FriendConfirmServlet.java
@@ -1,5 +1,7 @@
 package main.control;
 
+import main.JDBC.UserInfro;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,22 +9,31 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static main.JDBC.UserInfro.getUseridByUser_name;
+import static main.JDBC.UserInfro.getUseridByUsername;
 import static main.JDBC.friendconfirm.confirmfriend;
 
-@WebServlet("/FriendConfirmServlet")
+@WebServlet(name = "FriendConfirmServlet")
 public class FriendConfirmServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        String con_email_add = (String) request.getAttribute("con_email_button");
-        int  useremailid = (int) request.getAttribute("useremailid");
-        int applyemailid = (int) request.getAttribute("applyemailid");
-        if (con_email_add == "right"){
-            confirmfriend(useremailid, applyemailid);
-            response.sendRedirect("/confirm_sucess.jsp");
-        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        System.out.println("123");
+        String con_email_button = (String) request.getSession().getAttribute("con_email_button");
+        System.out.println(con_email_button);
+        String useremailname = (String) request.getSession().getAttribute("useremailname");
+        System.out.println(useremailname);
+        String applyemailname = (String) request.getSession().getAttribute("applyemailname");;
+        System.out.println(applyemailname);
+        int applyemailid = getUseridByUsername(applyemailname);
+        System.out.println(applyemailid);
+        int useremailid = getUseridByUser_name(useremailname);
+        System.out.println(useremailid);
+        if (useremailname != null && applyemailname!= null ){
+            confirmfriend(applyemailid,useremailid);
+            response.sendRedirect("/confirm_sucess.jsp");
+        }
     }
 }
