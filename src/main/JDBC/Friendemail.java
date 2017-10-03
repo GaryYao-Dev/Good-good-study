4360ab9976994b0fc69eb34aebf68cf1657f46df
ygy3389 <ygy3389@gmail.com>
2017-10-03 19:50:22 +1100

    Optimize Friendemail.java import

--- src/main/JDBC/Friendemail.java
+++ src/main/JDBC/Friendemail.java
@@ -1,16 +1,8 @@
 package main.JDBC;
-import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
-
 import javax.mail.*;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Properties;
 
 public class Friendemail {
