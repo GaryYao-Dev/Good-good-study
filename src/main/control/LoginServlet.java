0b18f024be6d880875eeb325b1213677b3455f31
jiewang-wj <31268741+jiewang-wj@users.noreply.github.com>
2017-10-03 22:15:20 +1100

    Add files via upload

--- src/main/control/LoginServlet.java
+++ src/main/control/LoginServlet.java
@@ -3,6 +3,7 @@ package main.control;
 import main.JDBC.DButil;
 import main.JDBC.UserInfro;
 import main.JDBC.login;
+import main.admin.admin_model;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -27,13 +28,21 @@ public class LoginServlet extends HttpServlet {
         String password = request.getParameter("password");
 
         Boolean login_confirm=  login.getlogin(username, password);
+        System.out.println(login_confirm);
         HttpSession session = request.getSession();
 
 
         if (login_confirm){
             int userid = UserInfro.getUseridByUsername(username);
             session.setAttribute("userid",userid);
+            System.out.println(userid);
             session.setAttribute("user_name",username);
+            admin_model am  = new admin_model();
+            try {
+                am.log_login(userid);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
             request.getRequestDispatcher("postmessage").forward(request,response);
         }else{
             session.setAttribute("login_confirm",login_confirm);
