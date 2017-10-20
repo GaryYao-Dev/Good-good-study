0ddc34697c76472fa51970f2df786b1bb44c7e74
wshhrm <wshhrm@163.com>
2017-10-20 21:16:24 +1100

    Update curation_servlet.java

--- src/main/admin/curation_servlet.java
+++ src/main/admin/curation_servlet.java
@@ -1,39 +1,39 @@
-//package main.admin;
-//
-//import javax.servlet.ServletContext;
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import java.io.PrintWriter;
-//import java.util.Set;
-//
-//@WebServlet("/curation")
-//public class curation_servlet extends HttpServlet{
-//    @Override
-//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        doPost(req, resp);
-//    }
-//
-//    @Override
-//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        ServletContext context = getServletContext();
-//        String realPath = context.getRealPath("/");
-//        String message = req.getParameter("message");
-//        curation_model curationModel= new curation_model(realPath);
-//        PrintWriter out = resp.getWriter();
-//
-//        try {
-//            Set<String> bully = curationModel.checkBully(1, message);
-//            for (String s:bully
-//                 ) {
-//                out.append(s+"\n");
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        out.close();
-//    }
-//}
+package main.admin;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Set;
+
+@WebServlet("/curation")
+public class curation_servlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext context = getServletContext();
+        String realPath = context.getRealPath("/");
+        String message = req.getParameter("message");
+        curation_model curationModel= new curation_model(realPath);
+        PrintWriter out = resp.getWriter();
+
+        try {
+            Set<String> bully = curationModel.checkBully(1, message);
+            for (String s:bully
+                 ) {
+                out.append(s+"\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        out.close();
+    }
+}
